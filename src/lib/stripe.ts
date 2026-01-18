0a22f31c5f13a64ed6afd8b9a5ca188a27491cde
jprtoledo <juan.prtoledo@gmail.com>
2026-01-18 00:45:33 -0300

    making env var optional

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,5 @@
 import 'server-only'
 
 import Stripe from 'stripe'
-const stripeSK = process.env.STRIPE_SECRET_KEY!
 
-export const stripe = new Stripe(stripeSK)
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '')
