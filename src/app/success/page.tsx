d32b0bd679758e4f0d743369d0b8f98866d3e955
jprtoledo <juan.prtoledo@gmail.com>
2026-01-23 15:52:00 -0300

    adding post purchase

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,12 +1,11 @@
 'use client'
 
 import {useEffect, useState} from "react";
-import { useRouter } from 'next/navigation';
 import env from "@/config/env";
+import {Loader2} from "lucide-react";
 
 export default function Success() {
-    const router = useRouter()
-    const [status, setStatus] = useState(null);
+    const [status, setStatus] = useState('');
     const [customerEmail, setCustomerEmail] = useState('');
 
     useEffect(() => {
@@ -22,21 +21,29 @@ export default function Success() {
             });
     }, []);
 
-    if (status === 'open') {
-        router.push("/checkout");
-    }
-
     if (status === 'complete') {
         return (
-            <section id="success">
-                <p>
-                    We appreciate your business! A confirmation email will be sent to {customerEmail}.
-
-                    If you have any questions, please email <a href="mailto:orders@example.com">orders@example.com</a>.
-                </p>
-            </section>
+            <div className="flex items-center justify-center min-h-dvh max-h-dvh">
+                <div className="flex flex-col items-center">
+                    <p className="pt-4 text-md font-bold text-foreground">Parabéns pela sua assinatura!</p>
+                    <p className="pt-4 text-sm text-muted-foreground">Um email com mais informações será enviado
+                        para {' '}
+                        <a className="font-bold" href={`mailto:${customerEmail}`}>{customerEmail}</a>
+                    </p>
+                    <img src={'/img/logo_light.png'} alt="Logo Formify" width={200}/>
+                </div>
+            </div>
         )
     }
 
-    return null;
+    return (
+        <div className="flex items-center justify-center min-h-dvh max-h-dvh">
+            <div className="flex flex-col items-center">
+                <Loader2 className="h-8 w-8 animate-spin text-black"/>
+                <p className="pt-4 text-md font-bold text-foreground">Sua assinatura está quase pronta!</p>
+                <p className="pt-4 text-sm text-muted-foreground">Aguarde mais um pouco. Estamos atualizando seus
+                    dados...</p>
+            </div>
+        </div>
+    )
 }
